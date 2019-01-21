2e889fe53e37bd7d6ed6eabf8584e7669300d353
Shivam Patidar <shivam.patidar@hashedin.com>
2019-01-21 15:42:23 +0530

    Initial Setup

---
+++ src/main/java/com/microfood/microfood/MicrofoodApplication.java
@@ -0,0 +1,14 @@
+package com.microfood.microfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrofoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrofoodApplication.class, args);
+	}
+
+}
+
